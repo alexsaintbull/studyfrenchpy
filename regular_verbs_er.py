e01b13ef05f4036416d8cfaaaf0947e27b2089f2
Ale Santoro <alexi.santoro@gmail.com>
2024-02-29 19:42:14 +0100

    more readable, added verbs, showing errors

--- regular_verbs_er.py
+++ regular_verbs_er.py
@@ -7,24 +7,31 @@ from verbs import reg_verbs_er as verbs, pronouns_er_conj as pronouns
 n = int(input("How many verbs should I ask you? "))
 
 corrects = 0
+errors = {}
 
 past_question = {}
 
 for i in range(n):
 
+	# generating verb and pronoun for the question
 	verb = choice(verbs)
 	pronoun = choice(list(pronouns))
 
+	# checking that the generated question isn't been asked already
 	if verb not in past_question:
 		past_question[verb] = list(pronoun)
 	else:
 		while pronoun in past_question[verb]:
+			# generating new pronoun if if the current one was already asked
 			pronoun = choice(list(pronouns))
 	
+	# added current question to the dictionary 
 	past_question[verb] = list(past_question[verb]) + list(pronoun)
 
+	# ask the question
 	answer = input( pronoun + " ____ (" + verb + "): "  )
 
+	# finding correct answer
 	if verb == "avoir":
 		rigth_answer = conj_avoir[pronoun]		
 	elif verb == "être":
@@ -32,11 +39,17 @@ for i in range(n):
 	else:
 		rigth_answer = verb.removesuffix("er")  + pronouns[pronoun]
 
+	# printing results
 	if answer == rigth_answer:
-		print("\nCORRECT! :)\n")
+		print("\n\tCORRECT! :)\n")
 		corrects += 1
 	else:
-		print("\nWRONG :(\n")
-		print("\t" + rigth_answer+"\n")
+		print("\n\tWRONG :(\n\n\t" + rigth_answer+"\n")
+		errors[answer] = rigth_answer
 
-print("Score: " + str(corrects) + "/" + str(n) + " (" + "{0:0.1f}".format(corrects*100/n) + "%)\n")
+# printing score and errors
+print("\n\nScore: " + str(corrects) + "/" + str(n) + " (" + "{0:0.1f}".format(corrects*100/n) + "%)\n")
+if not corrects == n:
+	print("See your errors:\n\tAnswer\t\tCorrect\n")
+	for a in errors:
+		print("\t"+a+":\t"+errors[a])
--- verbs.py
+++ verbs.py
@@ -1,7 +1,15 @@
-reg_verbs_er = ["adorer", "aimer", "arriver","avoir","détester","donner","être","habiter","monter","parler","réserver","rester"]
+
+# if future lists should get too long, consider using text files instead of lists to improve readibility
+reg_verbs_er = ["abaisser","adorer", "aimer", "arriver","avoir","détester","donner","être","habiter",
+"monter","parler","réserver","rester","abîmer","bouger","brosser","changer","chanter","chercher","commencer",
+"corriger","danser","demander","écouter","effacer","étudier","féliciter","fêter","gagner","glisser","jouer",
+"manger","manquer","marcher","nager","parler","partager","penser","regarder","skier","travailler","trouver","visiter","voyager"]
 
 pronouns_er_conj = {"je":"e", "tu":"es", "il/elle":"e", "nous":"ons", "vous":"ez", "ils/elles":"ent"}
 
 conj_avoir = {"je":"ai", "tu":"as", "il/elle":"a", "nous":"avons", "vous":"avez", "ils/elles":"ont"}
 conj_etre = {"je":"suis", "tu":"es", "il/elle":"est", "nous":"sommes", "vous":"êtes", "ils/elles":"sont"}
+
+if __name__ == '__main__':
+	print("\n\tThis is just a library of verbs for other files, it should not be run\n")
 
